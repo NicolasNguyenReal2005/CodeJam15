3a75247c5292eab1e7bbd4e2b9e2a2447bdc269a
Tristan Gray <tristan.dgl@gmail.com>
2025-11-15 17:54:19 -0500

    Added base code for the website extension

---
+++ script.js
@@ -0,0 +1,8 @@
+const analyzeButton = document.querySelector(".chat-send-btn");
+const chatAnalysis = document.getElementById("chat-analysis");
+analyzeButton.addEventListener("click", () => {
+
+    chatAnalysis.textContent = "Analyzing your argument for fallacies...";
+
+    const response = fetch()
+});
